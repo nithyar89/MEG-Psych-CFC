3afca96abcd192b6002ba0105594a06782ed4ba2
Nik Murphy <nikmurphyncluth@gmail.com>
2017-07-26 13:14:49 -0500

    Update meg_functions.py

--- meg_functions.py
+++ meg_functions.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 17 11:23:47 2017
-
-@author: nik
-"""
-
+# copyright Nik Murphy, PhD, Nithya Ramakrishnan, MS, CNTC, UTH, Houston 2017
 # MEG Pre-Processing Module
 # ENVIRONMENT
 print('Defining environment')
